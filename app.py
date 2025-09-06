961e250e355bbe993b83fb5bfea996c6c038164e
Murilo <murilojusti2006@gmail.com>
2025-09-06 00:45:13 -0300

    Criação da função 'transcribe'

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
+# Importando Bibliotecas
 import whisper
 
+# Define o modelo da I.A como "base"
 model = whisper.load_model("base")
+
+def transcribe(audio):
+    
+    #time.sleep(3)
+    # Carrega o áudio e faz ele ter 30s no máximo
+    audio = whisper.load_audio(audio)
+    audio = whisper.pad_or_trim(audio)
+
+    # Cria um espectograma na escala log-Mel e passa para o mesmo dispositivo do modelo
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    # Decodifica e configura o áudio
+    options = whisper.DecodingOptions(language="pt")
+    result = whisper.decode(model, mel, options)
+    
+    # Retorna a transcrição
+    return result.text
