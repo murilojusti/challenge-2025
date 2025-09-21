8c987ff37639d055f11e8650288a71f24f2455e4
Murilo <murilojusti2006@gmail.com>
2025-09-21 11:15:49 -0300

    Adicionando gemini para interpretar os comandos

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import whisper
 import gradio as gr
 import static_ffmpeg
 import pandas as pd
+from google import genai
+import os
+from dotenv import load_dotenv
 
-
+load_dotenv() # Carrega o env
 
 def transcribe(audio):
     
@@ -24,23 +27,61 @@ def transcribe(audio):
     # Retorna a transcrição
     return result.text
 
-# Adicionando ffmpeg aos paths do sistema
-static_ffmpeg.add_paths()
 
-# Define o modelo da I.A como "base"
-model = whisper.load_model("base")
+def interpretar_comando(transcrito):
+    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-# Cria interface web
-gr.Interface(
-    fn=transcribe,
-    inputs=gr.Audio(type="filepath"),
-    outputs=gr.Textbox(),
-    title="OpenAI Whisper ASR Gradio Web UI",
-    live=True
-).launch()
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents= f"""Você é um assistente que interpreta pedidos de controle de estoque hospitalar.
+                    Corrija eventuais erros de transcrição e retorne um JSON no seguinte formato:
 
-# Manipulação da planilha de estoque
-df = pd.read_excel('estoque.xlsx')
+                    {
+                      "acao": "adicionar ou retirar",
+                      "quantidade": número inteiro,
+                      "item": "nome do insumo"
+                    }
 
-# Salvar de volta
-df.to_excel("estoque.xlsx", index=False)
+                    Exemplo:
+                    Entrada: "dar baixa em duas seringas"
+                    Saída:
+                    {
+                      "acao": "retirar",
+                      "quantidade": 2,
+                      "item": "seringa"
+                    }
+
+                    Para 'acao' você deve retornar exatamente 'adicionar' ou 'retirar' dependendo do contexto.
+                    Para 'quantidade' você deve retornar a quantidade exata que entender no contexto
+                    Para 'item' você deve retornar uma dessas opções exatamente como está escrito a seguir: 'SERINGAS', 'GAZES', 'LUVAS', 'TUBOS_DE_COLETA'.
+                    
+                    Agora, interprete o seguinte {transcrito}
+                    """,
+    )
+
+    print(response.text)
+
+interpretar_comando('dar baixa em três tubos')
+
+# # Adicionando ffmpeg aos paths do sistema
+# static_ffmpeg.add_paths()
+
+# # Define o modelo da I.A como "base"
+# model = whisper.load_model("base")
+
+# # Cria interface web
+# gr.Interface(
+#     fn=transcribe,
+#     inputs=gr.Audio(type="filepath"),
+#     outputs=gr.Textbox(),
+#     title="OpenAI Whisper ASR Gradio Web UI",
+#     live=True
+# ).launch()
+
+# # Manipulação da planilha de estoque
+# df = pd.read_excel('estoque.xlsx')
+
+
+
+# # Salvar de volta
+# df.to_excel("estoque.xlsx", index=False)
